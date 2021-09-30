dabf527bea17e01ac89144936fda786ad0899d2d
manoninonam <manon.heijmans@gmail.com>
2021-09-30 15:11:23 +0200

    Updates

---
+++ precache-manifest.cbcb29cca18249381e1852c6fdb27025.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27e3d9f94b1bd1a82f45866937134bbd",
+    "url": "/live-preview-react-e-commerce-app/index.html"
+  },
+  {
+    "revision": "388133b83104455851cb",
+    "url": "/live-preview-react-e-commerce-app/static/css/main.b1179e46.chunk.css"
+  },
+  {
+    "revision": "5699d9886a658386837d",
+    "url": "/live-preview-react-e-commerce-app/static/js/2.d9e4d6e5.chunk.js"
+  },
+  {
+    "revision": "ea12c3c9e389454782f67b94c39904cd",
+    "url": "/live-preview-react-e-commerce-app/static/js/2.d9e4d6e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "388133b83104455851cb",
+    "url": "/live-preview-react-e-commerce-app/static/js/main.fb431dcb.chunk.js"
+  },
+  {
+    "revision": "7594b595efeb36da62d8",
+    "url": "/live-preview-react-e-commerce-app/static/js/runtime-main.7ecaf6d8.js"
+  },
+  {
+    "revision": "bde5ee944c453f0c9e15cf04be44a5e5",
+    "url": "/live-preview-react-e-commerce-app/static/media/YoungSerif-Regular.bde5ee94.otf"
+  }
+]);
